f0d7c2aa46e57e93e4c26bc1143a4be576fdde50
Rea WS <rwsnaydon@gmail.com>
2020-11-02 16:20:49 -0800

    Added option for correctly loading local apps with parent folders

--- gsheets/management/commands/syncgsheets.py
+++ gsheets/management/commands/syncgsheets.py
@@ -7,8 +7,15 @@ from gsheets import mixins
 class Command(BaseCommand):
     help = 'Finds all models mixing in a Sheet syncing mixin and executes the sync'
 
+    def add_arguments(self, parser):
+        # Named (optional) arguments
+        parser.add_argument(
+            '--apppath',
+            help='Find django apps nested under a relative path (path should be delineated with \'.\' instead of \'\\\')',
+        )
+
     def handle(self, *args, **options):
-        for model in self.find_syncable_models():
+        for model in self.find_syncable_models(options['apppath']):
             if issubclass(model, mixins.SheetSyncableMixin):
                 model.sync_sheet()
             elif issubclass(model, mixins.SheetPullableMixin):
@@ -22,13 +29,22 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS('Successfully finished sync'))
 
-    def find_syncable_models(self):
+    def find_syncable_models(self, app_path):
         app_models = []
         for app in settings.INSTALLED_APPS:
             try:
                 models = apps.get_app_config(app).get_models()
             except LookupError:
-                continue
+                if app_path:
+                    try:
+                        if app_path[-1] != '.':
+                            app_path += '.'
+                        app_name = app.split(app_path)[1]
+                        models = apps.get_app_config(app_name).get_models()
+                    except LookupError:
+                        continue
+                else:
+                    continue
 
             app_models += [m for m in models if issubclass(m, mixins.BaseGoogleSheetMixin)]
 
